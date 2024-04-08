f5c0417e0f5e920e66e1b22051ab2ba2d7fb4586
Rene Meza <renemeza.escamilla@gmail.com>
2024-04-08 02:35:45 -0700

    feat: add more categories and artist in filters

--- src/context/filter/filter.context.tsx
+++ src/context/filter/filter.context.tsx
@@ -2,7 +2,7 @@ import { createContextId } from "@builder.io/qwik";
 
 export type TFilter = {
   types: Array<"Shirt" | "Hoodie" | "Mug" | "Canvas" | "Poster">;
-  tag: Array<"Art" | "Political" | "Love">;
+  tag: Array<"Art" | "Political" | "Love" | "Way" | "Nasa">;
   artist: Array<"Hamit Gis" | "Wilfred Hildonen" | "Engin Selcuk">;
 };
 export interface filterProductState {
