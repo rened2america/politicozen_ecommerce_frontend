e515271863f649521aebf3002ed23c4e80470f8a
Raj <raj@d2america.com>
2024-09-07 13:58:38 +0530

    Try fix error

--- src/components/primitives/logo/logo.tsx
+++ src/components/primitives/logo/logo.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import logo from "/logo/logo_black.webp";
 
 export default component$(() => {
   return (
@@ -7,7 +6,7 @@ export default component$(() => {
       style={{ cursor: "pointer" }}
       width="39"
       height="44"
-      src={logo}
+      src={"/logo/logo_black.webp"}
       alt="logo"
     />
   );
