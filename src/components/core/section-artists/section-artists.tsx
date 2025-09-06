78961bf98c4d5624739593904bd2eee979e060e1
Raj <raj@d2america.com>
2025-09-06 20:53:06 +0530

    Minor fix to stop server crash when randomArts are not filled

--- src/components/core/section-artists/section-artists.tsx
+++ src/components/core/section-artists/section-artists.tsx
@@ -3,13 +3,16 @@ import Card from "../card-art/card-art";
 import { Link } from "@builder.io/qwik-city";
 
 export default component$((props: any) => {
+  if(!props.arts){
+    console.log("Please make sure random arts are generated!");
+  }
   return (
 
     <>
       <section class="bg-white relative">
         <div class="ruby0 flex justify-center items-center mb-[25px] relative z-[1] ml-[11rem] flex-wrap mx-auto">
           <div class="ruby grid pb-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {props.arts.map((art: any) => (
+            {props.arts?.map((art: any) => (
               <Link
                 key={art.id}
                 href={`/product/${art.id}/?size=17"x25.5"&product=Poster`} class="cursor-pointer">
