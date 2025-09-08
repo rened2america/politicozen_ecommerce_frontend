045a0d72f71aac23b1146dbb7455db631f78a4d1
Raj <raj@d2america.com>
2025-09-08 19:22:22 +0530

    how it works overhaul

--- src/routes/how-it-works/index.tsx
+++ src/routes/how-it-works/index.tsx
@@ -14,261 +14,106 @@ export default component$(() => {
   useStylesScoped$(styles);
   return (
     <section class="how-it-works">
-      <h1 class="main-title">Social Reality Punching Back</h1>
-      <h2 class="main-subtitle">
+      <div class="text-[24px] sm:text-[30px] md:text-[40px] lg:text-[60px] font-black flex justify-center text-center">
+        <span>Social Reality</span>
+        <span class="text-[#FFDA79] pl-3">Punching Back</span>
+      </div>
+      <div class="text-center text-[24px] sm:text-[30px] md:text-[40px] lg:text-[55px] font-black text-[#1A2445] opacity-20 leading-tight md:leading-[80px] lg:leading-[100px]">
         Unleash creativity, launch your Shop, make your mark!
-      </h2>
+      </div>
+
       <article class="content">
-        <div class="article-container">
-          <div
+
+        <div style="background-color: #203741;color: white; width: 100%; padding: 30px 0;">
+          <h2
+            class="subtitle"
             style={{
-              display: "grid",
-              justifyItems: "center",
+              color: "#ece8de",
             }}
           >
-            <h2
-              class="subtitle"
-              style={{
-                color: "#ece8de",
-              }}
-            >
-              Step-by-Step Guide
-            </h2>
-          </div>
-          <ol class="steps-list">
-            <li
-              style={{
-                display: "grid",
-                justifyItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  width: "180px",
-                  backgroundColor: "#ece8de",
-                  gridTemplateRows: "auto 1fr auto",
-                  borderRadius: "8px",
-                  padding: "24px",
-                  justifyItems: "center",
-                  textAlign: "center",
-                  gap: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "700",
-                    color: "#111",
-                  }}
-                >
-                  Sign Up & Set Up
-                </div>
-                <Image
-                  src="/howitworks/flame-1235.png"
-                  width="200"
-                  height="200"
-                  style={{
-                    alignSelf: "center",
-                    justifySelf: "center",
-                  }}
-                />
-                <div
-                  style={{
-                    fontWeight: "800",
-                    color: "#3a7b4d",
-                  }}
-                >
-                  Step 1
-                </div>
-              </div>
-            </li>
-            <li
-              style={{
-                display: "grid",
-                justifyItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  width: "180px",
-                  backgroundColor: "#ece8de",
-                  gridTemplateRows: "auto 1fr auto",
-                  borderRadius: "8px",
-                  padding: "24px",
-                  justifyItems: "center",
-                  textAlign: "center",
-                  gap: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "700",
-                    color: "#111",
-                  }}
-                >
-                  Upload Your Artwork
-                </div>
-                <Image src="/howitworks/upload.png" 
-                width="200" 
-                height="200" 
-                style={{
-                  alignSelf: "center",
-                  justifySelf: "center",
-                }}
-                />
-                <div
-                  style={{
-                    fontWeight: "800",
-                    color: "#3a7b4d",
-                  }}
-                >
-                  Step 2
-                </div>
-              </div>
-            </li>
-            <li
-              style={{
-                display: "grid",
-                justifyItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  width: "180px",
-                  backgroundColor: "#ece8de",
-                  gridTemplateRows: "auto 1fr auto",
-                  borderRadius: "8px",
-                  padding: "24px",
-                  justifyItems: "center",
-                  textAlign: "center",
-                  gap: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "700",
-                    color: "#111",
-                  }}
-                >
-                  Choose Your Merch
-                </div>
-                <Image
-                  src="/howitworks/selectStore.png"
-                  width="200"
-                  height="200"
-                  style={{
-                    alignSelf: "center",
-                    justifySelf: "center",
-                  }}
-                />
-                <div
-                  style={{
-                    fontWeight: "800",
-                    color: "#3a7b4d",
-                  }}
-                >
-                  Step 3
-                </div>
-              </div>
-            </li>
-            <li
-              style={{
-                display: "grid",
-                justifyItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  width: "180px",
-                  backgroundColor: "#ece8de",
-                  gridTemplateRows: "auto 1fr auto",
-                  borderRadius: "8px",
-                  padding: "24px",
-                  justifyItems: "center",
-                  textAlign: "center",
-                  gap: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "700",
-                    color: "#111",
-                  }}
-                >
-                  Customize Your Store
-                </div>
-                <Image src="/howitworks/choose.png"
-                  width="200"
-                  height="200"
-                  style={{
-                    alignSelf: "center",
-                    justifySelf: "center",
-                  }} />
-                <div
-                  style={{
-                    fontWeight: "800",
-                    color: "#3a7b4d",
-                  }}
-                >
-                  Step 4
-                </div>
-              </div>
-            </li>
-            <li
-              style={{
-                display: "grid",
-                justifyItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  width: "180px",
-                  backgroundColor: "#ece8de",
-                  gridTemplateRows: "auto 1fr auto",
-                  borderRadius: "8px",
-                  padding: "24px",
-                  justifyItems: "center",
-                  textAlign: "center",  
-                  gap: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "700",
-                    color: "#111",
-                  }}
-                >
-                  Promote & Sell
-                </div>
-                <Image src="/howitworks/promote.png"
-                  width="200"
-                  height="200"
-                  style={{
-                    alignSelf: "center",
-                    justifySelf: "center",
-                  }}
-                />
-                <div
-                  style={{
-                    fontWeight: "800",
-                    color: "#3a7b4d",
-                  }}
-                >
-                  Step 5
-                </div>
-              </div>
-            </li>
-          </ol>
+            Step-by-Step Guide
+          </h2>
         </div>
+        <div class="steps">
+          {/* step 1 */}
+          <div class="step">
+            <div class="step-left">
+              <img src="/howitworks/flame-1235.png" width="600"
+                height="500" alt="Step 1 Image" />
+                
+            </div>
+            
+            <div class="step-right">
+              <h2><span>01.</span> Sign Up & Set Up</h2>
+              <p>
+                Peaprint collects over <strong>1000+ products</strong> for your various customization needs.
+                Pick a product, considering its price, model variety, production location, etc.
+                It reduces shipping costs and fulfillment times from print providers to your customers.
+              </p>
+            </div>
+          </div>
+          {/* step 2 */}
+          <div class="step reverse">
+            <div class="step-left">
+              <img src="/howitworks/upload.png" width="600"
+                height="500" alt="Step 1 Image" />
+            </div>
+            <div class="step-right">
+              <h2><span>02.</span> Upload Your Artwork</h2>
+              <p>
+                Peaprint collects over <strong>1000+ products</strong> for your various customization needs.
+                Pick a product, considering its price, model variety, production location, etc.
+                It reduces shipping costs and fulfillment times from print providers to your customers.
+              </p>
+            </div>
+          </div>
+          {/* step 3 */}
+          <div class="step">
+            <div class="step-left">
+              <img src="/howitworks/selectStore.png" width="600"
+                height="500" alt="Step 1 Image" />
+            </div>
+            <div class="step-right">
+              <h2><span>03.</span> Choose Your Merch</h2>
+              <p>
+                Peaprint collects over <strong>1000+ products</strong> for your various customization needs.
+                Pick a product, considering its price, model variety, production location, etc.
+                It reduces shipping costs and fulfillment times from print providers to your customers.
+              </p>
+            </div>
+          </div>
+          {/* step 4 */}
+          <div class="step reverse">
+            <div class="step-left">
+              <img src="/howitworks/choose.png" width="600"
+                height="500" alt="Step 1 Image" />
+            </div>
+            <div class="step-right">
+              <h2><span>04.</span> Customize Your Store</h2>
+              <p>
+                Peaprint collects over <strong>1000+ products</strong> for your various customization needs.
+                Pick a product, considering its price, model variety, production location, etc.
+                It reduces shipping costs and fulfillment times from print providers to your customers.
+              </p>
+            </div>
+          </div>
+          {/* step 5 */}
+          <div class="step">
+            <div class="step-left">
+              <img src="/howitworks/promote.png" width="600"
+                height="500" alt="Step 1 Image" />
+            </div>
+            <div class="step-right">
+              <h2><span>05.</span> Promote & Sell</h2>
+              <p>
+                Peaprint collects over <strong>1000+ products</strong> for your various customization needs.
+                Pick a product, considering its price, model variety, production location, etc.
+                It reduces shipping costs and fulfillment times from print providers to your customers.
+              </p>
+            </div>
+          </div>
+        </div>
+
         <div
           style={{
             padding: "32px",
@@ -285,19 +130,19 @@ export default component$(() => {
                 justifyItems: "center",
               }}
             >
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
+              <div class="">
+                <div class="">
+                  <div class="">
                     <LuShoppingBag
                       style={{
                         fontSize: "32px",
-                        color: "black",
+                        color: "#FFDA79",
                         alignSelf: "end",
                       }}
                     />
                     <strong>Easy Online Selling</strong>
                   </div>
-                  <div class="flip-card-back">
+                  <div class="">
                     <p>
                       We take care of shipping, printing, and customer service.
                       Just focus on creating art.
@@ -312,19 +157,19 @@ export default component$(() => {
                 justifyItems: "center",
               }}
             >
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
+              <div >
+                <div >
+                  <div >
                     <LuCircleDollarSign
                       style={{
                         fontSize: "32px",
-                        color: "black",
+                        color: "#FFDA79",
                         alignSelf: "end",
                       }}
                     />
                     <strong>Earn Money</strong>
                   </div>
-                  <div class="flip-card-back">
+                  <div >
                     <p>
                       You get <strong>40% </strong>of the sales from your
                       products. No hidden fees.
@@ -339,19 +184,19 @@ export default component$(() => {
                 justifyItems: "center",
               }}
             >
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
+              <div >
+                <div >
+                  <div >
                     <LuUsers
                       style={{
                         fontSize: "32px",
-                        color: "black",
+                        color: "#FFDA79",
                         alignSelf: "end",
                       }}
                     />
                     <strong>Community & Collaboration</strong>
                   </div>
-                  <div class="flip-card-back">
+                  <div >
                     <p>
                       Be part of a vibrant network of political cartoonists.
                       Collaborate, learn, and grow together!
@@ -366,19 +211,19 @@ export default component$(() => {
                 justifyItems: "center",
               }}
             >
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
+              <div >
+                <div >
+                  <div >
                     <LuLineChart
                       style={{
                         fontSize: "32px",
-                        color: "black",
+                        color: "#FFDA79",
                         alignSelf: "end",
                       }}
                     />
                     <strong>Track Your Success</strong>
                   </div>
-                  <div class="flip-card-back">
+                  <div >
                     <p>
                       Use simple analytics to see how your store is doing and
                       plan for more sales.
@@ -393,19 +238,19 @@ export default component$(() => {
                 justifyItems: "center",
               }}
             >
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
+              <div >
+                <div >
+                  <div >
                     <LuShieldCheck
                       style={{
                         fontSize: "32px",
-                        color: "black",
+                        color: "#FFDA79",
                         alignSelf: "end",
                       }}
                     />
                     <strong>Get Paid Safely</strong>
                   </div>
-                  <div class="flip-card-back">
+                  <div >
                     <p>
                       Your earnings are securely transferred through our trusted
                       payment system.
